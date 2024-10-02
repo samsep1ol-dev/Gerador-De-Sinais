0c69840bd4d129eec4fd4015810d5cf89b8ff455
donkey03 <zblackoez@gmail.com>
2024-10-02 17:15:27 -0300

    Mudando de link de convite

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function gerarSinal() {
    
     const nh = new Date(Date.now() + tempoRestante * 1000);
     const nhStr = nh.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    const linkUrl = "https://9f.com/r/1ov2X6cb";
+    const linkUrl = "https://9f.com/r/j6p47Xia";
     const linkText = "Clique aqui para JOGAR e ganhar!";
     const ahref = `<a href="${linkUrl}" target="_blank">${linkText}</a>`;
     
